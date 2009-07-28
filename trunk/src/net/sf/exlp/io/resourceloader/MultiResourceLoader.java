fff8176e4d2176ae243b67d24f5f44f9423ea64c
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2009-07-28 05:50:43 +0000

    MultiResourceLoader added

---
+++ trunk/src/net/sf/exlp/io/resourceloader/MultiResourceLoader.java
@@ -0,0 +1,98 @@
+package net.sf.exlp.io.resourceloader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.apache.commons.lang.SystemUtils;
+import org.apache.log4j.Logger;
+
+public class MultiResourceLoader
+{
+	static Logger logger = Logger.getLogger(MultiResourceLoader.class);
+	
+	public enum LoadType {FileIs, Jws};
+	private static ArrayList<String> alLoadError;
+	private static ArrayList<String> alLoadDebug;
+	public static LoadType lastLT;
+	public static String lastAbsolutPath;
+	public static boolean debug = false;
+	
+	public MultiResourceLoader()
+	{
+		
+	}
+	
+	public synchronized InputStream searchIs(String resourceName) throws FileNotFoundException
+	{
+		ClassLoader cl = this.getClass().getClassLoader();
+		return searchIs(cl, resourceName);
+	}
+	
+	/**
+	 * 	 * @deprecated
+	 * @throws FileNotFoundException 
+	 */
+	public synchronized InputStream searchIs(ClassLoader cl, String resourceName) throws FileNotFoundException
+	{
+		alLoadError = new ArrayList<String>();
+		alLoadDebug = new ArrayList<String>();
+		InputStream is = null;
+		for(LoadType lt : LoadType.values())
+		{
+			switch (lt)
+			{
+				case FileIs: 	is=getFileIs(resourceName);break;
+				case Jws:		is=getJwsIs(cl,resourceName);break;
+			}
+			if(is!=null){lastLT=lt;break;}
+		}
+		if(debug){for(String s : alLoadDebug){logger.debug(s);}}
+		if(is==null)
+		{
+			for(String s : alLoadError){logger.debug(s);}
+			throw new FileNotFoundException("Missing File: "+resourceName);
+		}
+		return is;
+	}
+	
+	private InputStream getJwsIs(ClassLoader cl, String resourceName)
+	{
+		InputStream is=null;
+		resourceName = resourceName.replace(SystemUtils.FILE_SEPARATOR, "/");
+		URL url = cl.getResource(resourceName);
+		String confInfo = "ClassLoader.getResourceAsStream("+resourceName+")";
+		if(url!=null)
+		{
+			is = cl.getResourceAsStream(resourceName);
+			alLoadDebug.add("Found: "+confInfo+" "+url.getPath()+" "+is.getClass().getSimpleName()+".");
+		}
+		else
+		{
+			alLoadError.add("Not found: "+confInfo);
+		}
+		return is;
+	}
+	
+	private InputStream getFileIs(String resourceName)
+	{
+		InputStream is=null;
+		File f = new File(resourceName);
+		lastAbsolutPath = f.getAbsolutePath();
+		if(f.exists())
+		{
+			alLoadDebug.add("Gefunden: File("+lastAbsolutPath+")");
+			try {is = new FileInputStream(f);}
+			catch (FileNotFoundException e) {e.printStackTrace();}
+		}
+		else
+		{
+			alLoadError.add("Not found: File("+lastAbsolutPath+")");
+			f=null;
+		}
+		return is;
+	}
+}
