2be53e28d7446ddabc2b4573edcb06d75732e5f0
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2009-09-08 08:19:05 +0000

    EhVoid added .. EventHandler* renamed to Eh*

---
+++ trunk/src/net/sf/exlp/event/handler/EhVoid.java
@@ -0,0 +1,26 @@
+package net.sf.exlp.event.handler;
+
+import net.sf.exlp.event.AbstractEventHandler;
+import net.sf.exlp.event.LogEvent;
+
+import org.apache.log4j.Logger;
+
+
+
+public class EhVoid extends AbstractEventHandler 
+{
+	static Logger logger = Logger.getLogger(EhVoid.class);
+	static final long serialVersionUID=1;
+	
+	public EhVoid()
+	{
+
+	}
+
+	public boolean handleEvent(LogEvent pe)
+	{
+		count();
+		return true;
+	}
+
+}
