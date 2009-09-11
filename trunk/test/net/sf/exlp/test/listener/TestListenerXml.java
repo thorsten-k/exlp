60c80f525314f3dfa617eed574e7bc2927502eb4
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2009-09-11 08:43:17 +0000

    LogListenerXml added

---
+++ trunk/test/net/sf/exlp/test/listener/TestListenerXml.java
@@ -0,0 +1,28 @@
+package net.sf.exlp.test.listener;
+
+import net.sf.exlp.event.LogEventHandler;
+import net.sf.exlp.event.handler.EhDebug;
+import net.sf.exlp.io.LoggerInit;
+import net.sf.exlp.listener.LogListener;
+import net.sf.exlp.listener.impl.LogListenerXml;
+import net.sf.exlp.parser.LogParser;
+import net.sf.exlp.parser.impl.TestParser;
+
+import org.apache.log4j.Logger;
+
+public class TestListenerXml
+{
+	static Logger logger = Logger.getLogger(TestListenerXml.class);
+	
+	public static void main(String args[])
+	{
+		LoggerInit loggerInit = new LoggerInit("log4j.xml");	
+			loggerInit.addAltPath("resources/config");
+			loggerInit.init();
+			
+		LogEventHandler leh = new EhDebug();
+		LogParser lp = new TestParser(leh);
+		LogListener ll = new LogListenerXml("../openFuXML/resources/data/timeline.xml",lp);
+		ll.processMulti("/wikiinjection/a");
+	}
+}
