3b662a7eb77890b171bc4a89eac8d19dac55692b
thorsten-k <t.kisner@web.de>
2009-09-22 10:07:46 +0000

    Debug Output for all Jaxb-Classes

--- src/net/sf/exlp/util/JaxbUtil.java
+++ src/net/sf/exlp/util/JaxbUtil.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
 import net.sf.exlp.io.resourceloader.MultiResourceLoader;
@@ -27,5 +28,26 @@ public class JaxbUtil
 		catch (JAXBException e) {logger.error(e);}
 		catch (FileNotFoundException e) {logger.error(e);}
 		return result;
+	}
+	
+	public static synchronized void debug(Object jaxb)
+	{
+		debug(jaxb, null);
+	}
+	
+	public static synchronized void debug(Object jaxb, Object nsPrefixMapper)
+	{
+		try
+		{
+			JAXBContext context = JAXBContext.newInstance(jaxb.getClass());
+			Marshaller m = context.createMarshaller(); 
+			m.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE );
+			if(nsPrefixMapper!=null)
+			{
+				m.setProperty("com.sun.xml.bind.namespacePrefixMapper",nsPrefixMapper);
+			}
+			m.marshal( jaxb, System.out);
+		}
+		catch (JAXBException e) {logger.error(e);}
 	}
 }
