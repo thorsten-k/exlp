29470a20d6d98acc7f2a1c67f6355333a4a129dc
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2010-01-20 09:30:11 +0000

    Facade Queries with double[] added. ArchUtil added

--- trunk/src/net/sf/exlp/addon/apache/facade/bean/ExlpApacheFacadeBean.java
+++ trunk/src/net/sf/exlp/addon/apache/facade/bean/ExlpApacheFacadeBean.java
@@ -1,9 +1,9 @@
 package net.sf.exlp.addon.apache.facade.bean;
 
 import java.io.Serializable;
+import java.math.BigInteger;
 
 import javax.ejb.Remote;
-import javax.ejb.Remove;
 import javax.ejb.Stateful;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
@@ -32,6 +32,65 @@ public class ExlpApacheFacadeBean extends AbstractExlpFacadeBean implements Exlp
 		return apache;
 	}
 	
-	@Remove
-	public void checkout(){logger.debug("Checkout");}	
+	public double[][] dHitsPerMonth(Double minYear)
+	{	
+		double[][] result;
+		StringBuffer sb = new StringBuffer();
+			sb.append("SELECT year(record) AS y,month(record) AS m,count(id)");
+			sb.append(" FROM "+ExlpApache.class.getSimpleName());
+			sb.append(" WHERE year(record) >= :minYear");
+			sb.append(" GROUP BY y,m");
+			sb.append(" ORDER BY y,m");
+		Query query=getManager().createNativeQuery(sb.toString());
+			query.setParameter("minYear", minYear.intValue());
+		Object[] rows;
+		try{rows = query.getResultList().toArray();}
+		catch (NoResultException ex)
+		{
+			result = new double[0][0];
+			return result;
+		}
+		
+		result = new double[3][rows.length];
+		for(int i=0;i<rows.length;i++)
+		{
+			Object[] row = (Object[])rows[i];
+			result[0][i] = ((Integer)row[0]).doubleValue();
+			result[1][i] = ((Integer)row[1]).doubleValue();
+			result[2][i] = ((BigInteger)row[2]).doubleValue();
+		}
+		
+		return result;
+	}
+	
+	public double[][] dHitsPerDay(Double minYear)
+	{	
+		double[][] result;
+		StringBuffer sb = new StringBuffer();
+			sb.append("SELECT year(record) AS y,month(record) AS m,day(record) AS d,count(id)");
+			sb.append(" FROM "+ExlpApache.class.getSimpleName());
+			sb.append(" WHERE year(record) >= :minYear");
+			sb.append(" GROUP BY y,m,d");
+			sb.append(" ORDER BY y,m,d");
+		Query query=getManager().createNativeQuery(sb.toString());
+			query.setParameter("minYear", minYear.intValue());
+		Object[] rows;
+		try{rows = query.getResultList().toArray();}
+		catch (NoResultException ex)
+		{
+			result = new double[0][0];
+			return result;
+		}
+		
+		result = new double[4][rows.length];
+		for(int i=0;i<rows.length;i++)
+		{
+			Object[] row = (Object[])rows[i];
+			result[0][i] = ((Integer)row[0]).doubleValue();
+			result[1][i] = ((Integer)row[1]).doubleValue();
+			result[2][i] = ((Integer)row[2]).doubleValue();
+			result[3][i] = ((BigInteger)row[3]).doubleValue();
+		}
+		return result;
+	}
 }
