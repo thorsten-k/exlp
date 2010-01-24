df8dfee26e95728398c6b09149a0f510fab258fc
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2010-01-24 11:37:11 +0000

    EhPersist re-activated

--- trunk/src/net/sf/exlp/event/handler/EhPersist.java
+++ trunk/src/net/sf/exlp/event/handler/EhPersist.java
@@ -1,9 +1,11 @@
 package net.sf.exlp.event.handler;
 
+import java.util.Hashtable;
 import java.util.Map;
 
 import net.sf.exlp.event.AbstractEventHandler;
 import net.sf.exlp.event.LogEvent;
+import net.sf.exlp.event.exception.HandlerException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -14,6 +16,11 @@ public class EhPersist extends AbstractEventHandler
 	static final long serialVersionUID=1;
 		
 	Map<String,Object> facades;
+	
+	public EhPersist()
+	{
+		facades = new Hashtable<String,Object>();
+	}
 	
 	public EhPersist(Map<String,Object> facades)
 	{
@@ -26,5 +33,10 @@ public class EhPersist extends AbstractEventHandler
 		count();
 		boolean result = le.persist(facades);
 		return result;
+	}
+	
+	public void addFacade(Class<?> c,Object facade)
+	{
+		facades.put(c.getSimpleName(), facade);
 	}
 }
