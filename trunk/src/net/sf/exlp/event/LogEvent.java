5815521ac809cf185d0762b3a70e29f0036e56c1
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2010-01-24 08:36:33 +0000

    ICMP Ping (shell) added

--- trunk/src/net/sf/exlp/event/LogEvent.java
+++ trunk/src/net/sf/exlp/event/LogEvent.java
@@ -1,9 +1,11 @@
 package net.sf.exlp.event;
 
+import java.util.Date;
 import java.util.Map;
 
 public interface LogEvent
 {
 	void debug();
+	Date getRecord();
 	boolean persist(Map<String,?> facades);
 }
