90ed5ecf82c2ed6e4d5a236b4cfcf7cf89705314
thorsten-k <t.kisner@web.de>
2011-02-01 06:24:14 +0000

    DirTreeScanner now supports Dir/File, DirFile and ExlpFile

--- test/net/sf/exlp/test/addon/dirsize/TestDirTreeScanner.java
+++ test/net/sf/exlp/test/addon/dirsize/TestDirTreeScanner.java
@@ -2,8 +2,9 @@ package net.sf.exlp.test.addon.dirsize;
 
 import java.io.FileNotFoundException;
 
-import net.sf.exlp.addon.dirsize.data.factory.DirToDirFileFactory;
+import net.sf.exlp.addon.dirsize.data.ejb.ExlpFile;
 import net.sf.exlp.addon.dirsize.data.jaxb.Dir;
+import net.sf.exlp.addon.dirsize.data.jaxb.DirFile;
 import net.sf.exlp.addon.dirsize.util.DirTreeScanner;
 import net.sf.exlp.io.LoggerInit;
 import net.sf.exlp.util.xml.JaxbUtil;
@@ -23,9 +24,19 @@ public class TestDirTreeScanner
 	public void scan() throws FileNotFoundException
 	{
 		DirTreeScanner scanner = new DirTreeScanner();
-		Dir dir = scanner.getDirTree("/Users/thorsten/Documents/workspace/3.6.0/ExLP/dist");
+		Dir dir = scanner.getDir("/Users/thorsten/Documents/workspace/3.6.0/ExLP/dist");
 		JaxbUtil.debug(dir);
-		JaxbUtil.debug(DirToDirFileFactory.convert(dir));
+		DirFile df = scanner.getDirFile("/Users/thorsten/Documents/workspace/3.6.0/ExLP/dist");
+		JaxbUtil.debug(df);
+		
+		ExlpFile ef = scanner.getExlpFile("/Users/thorsten/Documents/workspace/3.6.0/ExLP/dist");
+		logger.debug("Debug "+ef.getChilds().size());
+		for (int i=0;i<ef.getChilds().size();i++)
+		{
+			logger.debug(i);
+			ExlpFile cf = ef.getChilds().get(i);
+			logger.debug("\t"+cf.getName());
+		}
 	}
 	
 	public static void main (String[] args) throws Exception
