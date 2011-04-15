9d080a88ba2ff249e9941a1cb25aeff8a14dc700
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2011-04-15 09:29:14 +0000

    Factory creates relative paths for filenames

---
+++ trunk/util/src/main/java/net/sf/exlp/util/io/RelativePathFactory.java
@@ -0,0 +1,61 @@
+package net.sf.exlp.util.io;
+
+import java.io.File;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class RelativePathFactory
+{
+	static Log logger = LogFactory.getLog(RelativePathFactory.class);
+	
+	private boolean quoteSpaces;
+	
+	public RelativePathFactory(){this(true);}
+	public RelativePathFactory(boolean quoteSpaces)
+	{
+		this.quoteSpaces=quoteSpaces;
+	}
+
+	public String relativate(File fullFixed, File fullRelative)
+	{
+		return relativate(fullFixed.getAbsolutePath(), fullRelative.getAbsolutePath());
+	}
+	
+	public String relativate(String fullFixed, String fullRelative)
+	{
+		String fNormalized = FilenameUtils.normalize(fullFixed, true);
+		String rNormalized = FilenameUtils.normalize(fullRelative, true);
+		 
+		String relative = getRelative( fNormalized,  rNormalized);
+		
+		if(quoteSpaces){relative = quoteSpaces(relative);}
+		relative = FilenameUtils.separatorsToSystem(relative);
+		
+		return relative;
+	}
+	
+	private String getRelative(String fNormalized, String rNormalized)
+	{
+		String result = rNormalized;
+		int index = rNormalized.indexOf(fNormalized);
+		
+		if(index==0)
+		{
+			result = rNormalized.substring(fNormalized.length(), rNormalized.length());
+		}		
+		return result;
+	}
+	
+	private String quoteSpaces(String path)
+	{
+		if(path.contains(" "))
+		{
+			path = "\""+path+"\"";
+		}
+		
+		
+		return path;
+	}
+}
