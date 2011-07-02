f23febbbc908c2dea785082f999c3fa554941654
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2011-07-02 23:01:12 +0000

    applied commons-jxpath

--- trunk/util/src/main/java/net/sf/exlp/xml/xpath/NetXpath.java
+++ trunk/util/src/main/java/net/sf/exlp/xml/xpath/NetXpath.java
@@ -2,57 +2,34 @@ package net.sf.exlp.xml.xpath;
 
 import java.util.List;
 
-import net.sf.exlp.util.exception.ExlpXpathNotFoundException;
 import net.sf.exlp.util.exception.ExlpXpathNotUniqueException;
-import net.sf.exlp.util.xml.JDomUtil;
-import net.sf.exlp.util.xml.JaxbUtil;
 import net.sf.exlp.xml.net.Url;
 import net.sf.exlp.xml.net.Urls;
-import net.sf.exlp.xml.ns.ExlpNsPrefixMapper;
-import net.sf.exlp.xml.ns.NsPrefixMapperInterface;
 
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.JXPathNotFoundException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.xpath.XPath;
 
 public class NetXpath
 {
 	static Log logger = LogFactory.getLog(NetXpath.class);
 	
-	private static NsPrefixMapperInterface nsPrefixMapper;
-	
-	public static synchronized Url getUrl(List<Url> listUrl, String code) throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+	public static synchronized Url getUrl(List<Url> listUrl, String code) throws JXPathNotFoundException, ExlpXpathNotUniqueException
 	{
 		Urls urls = new Urls();
 		urls.getUrl().addAll(listUrl);
         return getUrl(urls, code);
 	}
 	
-	@SuppressWarnings("unchecked")
-	public static synchronized Url getUrl(Urls urls, String code) throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+	public static synchronized Url getUrl(Urls urls, String code) throws JXPathNotFoundException, ExlpXpathNotUniqueException
 	{
-		Url url = null;
-		try
-		{
-			Document doc = JaxbUtil.toDocument(urls, getNsPrefixMapper());
-			XPath xpath = XPath.newInstance( "//net:url[@code='"+code+"']");
-			xpath.addNamespace("net", "http://exlp.sf.net/net");
-			List<Object> list = xpath.selectNodes(doc);
-			if(list.size()==0){throw new ExlpXpathNotFoundException("No url found for code="+code);}
-			else if(list.size()>1){throw new ExlpXpathNotUniqueException("No unique url for code="+code);}
-			Element e = (Element)list.get(0);
-			url = (Url)JDomUtil.toJaxb(e, Url.class);
-		}
-		catch (JDOMException e) {logger.error(e);e.printStackTrace();}
-        return url;
-	}
-	
-	private static NsPrefixMapperInterface getNsPrefixMapper()
-	{
-		if(nsPrefixMapper==null){nsPrefixMapper = new ExlpNsPrefixMapper();}
-		return nsPrefixMapper; 
+		JXPathContext context = JXPathContext.newContext(urls);
+		
+		@SuppressWarnings("unchecked")
+		List<Url> list = (List<Url>)context.selectNodes("url[@code='"+code+"']");
+		if(list.size()==0){throw new JXPathNotFoundException("No URL for code="+code);}
+		else if(list.size()>1){throw new ExlpXpathNotUniqueException("Multiple URLs for code="+code);}
+		return list.get(0);
 	}
 }
---
+++ trunk/util/src/test/java/net/sf/exlp/test/xml/xpath/net/TestNetXpathUrl.java
@@ -0,0 +1,123 @@
+package net.sf.exlp.test.xml.xpath.net;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.exlp.util.exception.ExlpXpathNotFoundException;
+import net.sf.exlp.util.exception.ExlpXpathNotUniqueException;
+import net.sf.exlp.util.io.LoggerInit;
+import net.sf.exlp.util.xml.JaxbUtil;
+import net.sf.exlp.xml.net.Url;
+import net.sf.exlp.xml.net.Urls;
+import net.sf.exlp.xml.xpath.NetXpath;
+
+import org.apache.commons.jxpath.JXPathNotFoundException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestNetXpathUrl
+{
+	static Log logger = LogFactory.getLog(TestNetXpathUrl.class);
+	
+	private static Url url1,url2,url3,url4;
+	
+	public TestNetXpathUrl()
+	{
+
+	}
+	
+	@BeforeClass
+    public static void initLogger()
+	{
+		LoggerInit loggerInit = new LoggerInit("log4junit.xml");	
+		loggerInit.addAltPath("src/test/resources/config");
+		loggerInit.init();
+    }
+	
+	@BeforeClass
+	public static void initUrls()
+	{
+		url1 = new Url();
+    	url1.setCode("code1");
+    	url1.setValue("http://url1");
+    	
+    	url2 = new Url();
+    	url2.setCode("code2");
+    	url2.setValue("http://url1");
+    	
+    	url3 = new Url();
+    	url3.setCode("code3");
+    	url3.setValue("http://url3");
+    	
+    	url4 = new Url();
+    	url4.setCode("code3");
+    	url4.setValue("http://url4");
+	}
+    
+	private Urls createUrls()
+    {
+    	Urls urls = new Urls();
+    	urls.getUrl().add(url1);
+    	urls.getUrl().add(url2);
+    	urls.getUrl().add(url3);
+    	urls.getUrl().add(url4);
+    	return urls;
+    }
+	
+    @Test
+    public void testCode1() throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+    {
+    	Urls urls = createUrls();
+    	Url url = NetXpath.getUrl(urls, "code1");
+    	Assert.assertEquals(JaxbUtil.toString(url1),JaxbUtil.toString(url));
+    }
+    
+    @Test
+    public void testCode2() throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+    {
+    	Urls urls = createUrls();
+    	Url url = NetXpath.getUrl(urls, "code2");
+    	Assert.assertEquals(JaxbUtil.toString(url2),JaxbUtil.toString(url));
+    }
+
+    @Test(expected=JXPathNotFoundException.class)
+    public void testNotFound() throws JXPathNotFoundException, ExlpXpathNotUniqueException
+    {
+    	Urls urls = createUrls();
+    	Url url = NetXpath.getUrl(urls, "code0");
+    	Assert.assertEquals(JaxbUtil.toString(url2),JaxbUtil.toString(url));
+    }
+    
+    @Test(expected=ExlpXpathNotUniqueException.class)
+    public void testUnique() throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+    {
+    	Urls urls = createUrls();
+    	NetXpath.getUrl(urls, "code3");
+    }
+    
+    @Test
+    public void testList() throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+    {
+    	List<Url> list = new ArrayList<Url>();
+    	list.add(url1);
+    	list.add(url2);
+    	list.add(url3);
+    	Urls urls = createUrls();
+    	Url url = NetXpath.getUrl(urls, "code2");
+    	Assert.assertEquals(JaxbUtil.toString(url2),JaxbUtil.toString(url));
+    }
+    
+	public static void main(String[] args) throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+    {
+		LoggerInit loggerInit = new LoggerInit("log4j.xml");	
+			loggerInit.addAltPath("src/test/resources/config");
+			loggerInit.init();	
+		
+		TestNetXpathUrl test = new TestNetXpathUrl();
+		TestNetXpathUrl.initUrls();
+		test.testUnique();
+    }
+}
