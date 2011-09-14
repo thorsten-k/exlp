404c10f3a31680515d6ab4fcfa7a3e75cfa40794
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2011-09-14 12:11:02 +0000

    Test  for Certificate added

--- trunk/util/src/test/java/net/sf/exlp/xml/io/TestPolicy.java
+++ trunk/util/src/test/java/net/sf/exlp/xml/io/TestPolicy.java
@@ -2,38 +2,31 @@ package net.sf.exlp.xml.io;
 
 import java.io.FileNotFoundException;
 
-import net.sf.exlp.test.AbstractExlpTest;
 import net.sf.exlp.util.io.LoggerInit;
 import net.sf.exlp.util.xml.JaxbUtil;
-import net.sf.exlp.xml.io.Policy;
 import net.sf.exlp.xml.ns.ExlpNsPrefixMapper;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TestPolicy extends AbstractExlpTest
+public class TestPolicy extends AbstractIoXmlTest
 {
 	static Log logger = LogFactory.getLog(TestPolicy.class);
-	
-	private static final String rootDir = "src/test/resources/data/xml/io";
-	
-	private static java.io.File fPolicy;
-	
+		
 	@BeforeClass
 	public static void initFiles()
 	{
-		fPolicy = new java.io.File(rootDir,"policy.xml");
+		fXml = new java.io.File(rootDir,"policy.xml");
 	}
     
     @Test
     public void testFile() throws FileNotFoundException
     {
     	Policy test = createPolicy();
-    	Policy ref = (Policy)JaxbUtil.loadJAXB(fPolicy.getAbsolutePath(), Policy.class);
-    	Assert.assertEquals(JaxbUtil.toString(ref),JaxbUtil.toString(test));
+    	Policy ref = (Policy)JaxbUtil.loadJAXB(fXml.getAbsolutePath(), Policy.class);
+    	assertJaxbEquals(ref, test);
     }
     
     public void save()
@@ -41,7 +34,7 @@ public class TestPolicy extends AbstractExlpTest
     	logger.debug("Saving Reference XML");
     	Policy xml = createPolicy();
     	JaxbUtil.debug2(this.getClass(),xml, new ExlpNsPrefixMapper());
-    	JaxbUtil.save(fPolicy, xml, new ExlpNsPrefixMapper(), true);
+    	JaxbUtil.save(fXml, xml, new ExlpNsPrefixMapper(), true);
     }
     
     public static Policy createPolicy()
