3ef140e254bf3b7d62f1ad13c9a398dc4bade175
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2011-12-26 08:19:06 +0000

    RecursiveFileFinder added with Test

--- trunk/util/src/main/java/net/sf/exlp/util/io/dir/RecursiveFileFinder.java
+++ trunk/util/src/main/java/net/sf/exlp/util/io/dir/RecursiveFileFinder.java
@@ -8,18 +8,22 @@ import java.util.Collection;
 import java.util.List;
 
 import org.apache.commons.io.DirectoryWalker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class RecursiveFileRemover extends DirectoryWalker<File>
+public class RecursiveFileFinder extends DirectoryWalker<File>
 {
+	final static Logger logger = LoggerFactory.getLogger(RecursiveFileFinder.class);
+	
 	private FileFilter filter;
 	
-	public RecursiveFileRemover(FileFilter filter)
+	public RecursiveFileFinder(FileFilter filter)
     {
 		super();
 		this.filter=filter;
     }
 
-	public List<File> clean(File startDirectory) throws IOException
+	public List<File> find(File startDirectory) throws IOException
 	{
 		List<File> results = new ArrayList<File>();
 		walk(startDirectory, results);
@@ -35,7 +39,6 @@ public class RecursiveFileRemover extends DirectoryWalker<File>
     {
     	if(filter.accept(file))
     	{
-    		file.delete();
     		results.add(file);
     	}
     }
