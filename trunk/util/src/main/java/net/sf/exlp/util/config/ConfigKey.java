776bf1be4a7e81e7e025626a15be5c74b5cb8889
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2012-08-31 08:06:20 +0000

    ConfigKey added. ConfigLoader moved to config package

---
+++ trunk/util/src/main/java/net/sf/exlp/util/config/ConfigKey.java
@@ -0,0 +1,10 @@
+package net.sf.exlp.util.config;
+
+public interface ConfigKey
+{
+	public final static String netRestUrl ="net.rest.url";
+	
+	public final static String netEjbHost ="net.ejb.host";
+	public final static String netEjbUser ="net.ejb.user";
+	public final static String netEjbPwd  ="net.ejb.pwd";
+}
