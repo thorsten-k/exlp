7d2d5dce291dd62a08a90894e47c7f5769ad053a
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2012-10-15 15:15:38 +0000

    IMAP ConfigKeys added

--- trunk/util/src/main/java/net/sf/exlp/util/config/ConfigKey.java
+++ trunk/util/src/main/java/net/sf/exlp/util/config/ConfigKey.java
@@ -7,4 +7,8 @@ public interface ConfigKey
 	public final static String netEjbHost ="net.ejb.host";
 	public final static String netEjbUser ="net.ejb.user";
 	public final static String netEjbPwd  ="net.ejb.pwd";
+	
+	public final static String netImapHost  ="net.imap.host";
+	public final static String netImapUser  ="net.imap.user";
+	public final static String netImapPwd  ="net.imap.pwd";
 }
