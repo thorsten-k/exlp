73e8fed8a7955905dea88218ee4af6a215ac7d02
thorsten-k <t.kisner@web.de>
2012-10-18 06:29:56 +0000

    ConfigKeys updated for smtp

--- util/src/main/java/net/sf/exlp/util/config/ConfigKey.java
+++ util/src/main/java/net/sf/exlp/util/config/ConfigKey.java
@@ -9,6 +9,12 @@ public interface ConfigKey
 	public final static String netEjbPwd  ="net.ejb.pwd";
 	
 	public final static String netImapHost  ="net.imap.host";
+	public final static String netImapPort  ="net.imap.port";
 	public final static String netImapUser  ="net.imap.user";
 	public final static String netImapPwd  ="net.imap.pwd";
+	
+	public final static String netSmtpHost  ="net.smtp.host";
+	public final static String netSmtpPort  ="net.smtp.port";
+	public final static String netSmtpUser  ="net.smtp.user";
+	public final static String netSmtpPwd  ="net.smtp.pwd";
 }
