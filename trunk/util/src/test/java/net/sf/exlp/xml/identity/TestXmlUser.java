1ba69617c29f8770c1e3ab15d6829270177e7753
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2012-12-30 09:13:21 +0000

    password added to id:user

--- trunk/util/src/test/java/net/sf/exlp/xml/identity/TestXmlUser.java
+++ trunk/util/src/test/java/net/sf/exlp/xml/identity/TestXmlUser.java
@@ -2,18 +2,18 @@ package net.sf.exlp.xml.identity;
 
 import java.io.FileNotFoundException;
 
+import net.sf.exlp.test.ExlpTstBootstrap;
 import net.sf.exlp.util.io.LoggerInit;
 import net.sf.exlp.util.xml.JaxbUtil;
-import net.sf.exlp.xml.ns.ExlpNsPrefixMapper;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TestUser extends AbstractIdentityXmlTest
+public class TestXmlUser extends AbstractIdentityXmlTest
 {
-	final static Logger logger = LoggerFactory.getLogger(TestUser.class);
+	final static Logger logger = LoggerFactory.getLogger(TestXmlUser.class);
 		
 	@BeforeClass
 	public static void initFiles()
@@ -25,7 +25,7 @@ public class TestUser extends AbstractIdentityXmlTest
     public void testFile() throws FileNotFoundException
     {
     	User test = createUser();
-    	User ref = (User)JaxbUtil.loadJAXB(fXml.getAbsolutePath(), User.class);
+    	User ref = JaxbUtil.loadJAXB(fXml.getAbsolutePath(), User.class);
     	assertJaxbEquals(ref, test);
     }
     
@@ -43,6 +43,7 @@ public class TestUser extends AbstractIdentityXmlTest
     	xml.setFirstName("myFirst");
     	xml.setLastName("myLast");
     	xml.setAccount("myAcc");
+    	xml.setPassword("myPwd");
     	xml.setEmail("my@e.mail");
     	xml.setInfo("myInfo");
     	return xml;
@@ -50,12 +51,10 @@ public class TestUser extends AbstractIdentityXmlTest
 	
 	public static void main(String[] args)
     {
-		LoggerInit loggerInit = new LoggerInit("log4j.xml");	
-			loggerInit.addAltPath("src/test/resources/config");
-			loggerInit.init();		
+		ExlpTstBootstrap.init();
 			
-		TestUser.initFiles();	
-		TestUser test = new TestUser();
+		TestXmlUser.initFiles();	
+		TestXmlUser test = new TestXmlUser();
 		test.save();
     }
 }
