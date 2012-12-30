83ad1e9c80afae9124520188ed6e5159fd0b7d91
thorsten-k <t.kisner@web.de>
2012-12-30 07:59:02 +0000

    XML net:host added

--- util/src/test/java/net/sf/exlp/test/ExlpTstBootstrap.java
+++ util/src/test/java/net/sf/exlp/test/ExlpTstBootstrap.java
@@ -1,6 +1,8 @@
 package net.sf.exlp.test;
 
 import net.sf.exlp.util.io.LoggerInit;
+import net.sf.exlp.util.xml.JaxbUtil;
+import net.sf.exlp.xml.ns.ExlpNsPrefixMapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +14,8 @@ public class ExlpTstBootstrap
 	public static void init()
 	{
 		LoggerInit loggerInit = new LoggerInit("log4j.xml");
-		loggerInit.addAltPath("exlp-util.test");
-		loggerInit.init();
+			loggerInit.addAltPath("exlp-util.test");
+			loggerInit.init();
+		JaxbUtil.setNsPrefixMapper(new ExlpNsPrefixMapper());
 	}
 }
