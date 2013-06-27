bafe02a859f7cf371e7ed0b73774687f9f230f2b
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2013-06-27 12:23:06 +0000

    hibernate-entitymanager added

--- trunk/monitor/src/main/java/net/sf/exlp/monitor/model/MonitoringTestSeries.java
+++ trunk/monitor/src/main/java/net/sf/exlp/monitor/model/MonitoringTestSeries.java
@@ -1,13 +1,49 @@
 package net.sf.exlp.monitor.model;
 
-import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "contact")
 public class MonitoringTestSeries
 {
-	private Date record;
-	
-	public MonitoringTestSeries()
-	{
-		record = new Date();
-	}
+	    private Integer id;
+	    private String name;
+	    private String email;
+	 
+	    public MonitoringTestSeries() {
+	 
+	    }
+	 
+	    public MonitoringTestSeries(Integer id, String name, String email) {
+	        this.id = id;
+	        this.name = name;
+	        this.email = email;
+	    }
+	 
+	    @Id
+	    public Integer getId() {
+	        return this.id;
+	    }
+	 
+	    public void setId(Integer id) {
+	        this.id = id;
+	    }
+	 
+	    public String getName() {
+	        return this.name;
+	    }
+	 
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+	 
+	    public String getEmail() {
+	        return email;
+	    }
+	 
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
 }
---
+++ trunk/monitor/src/test/java/net/sf/exlp/monitor/TestHibernate.java
@@ -0,0 +1,49 @@
+package net.sf.exlp.monitor;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import net.sf.ahtutils.controller.facade.UtilsFacadeBean;
+import net.sf.exlp.monitor.model.MonitoringTestSeries;
+import net.sf.exlp.test.ExlpMonitorTestBootstrap;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.service.ServiceRegistry;
+
+public class TestHibernate
+{
+	private static SessionFactory sessionFactory = null;  
+    private static ServiceRegistry serviceRegistry = null;  
+       
+  
+     
+    public static void main(String[] args)
+    {
+    	ExlpMonitorTestBootstrap.init();
+    	
+       
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("exlp");
+        EntityManager em = emf.createEntityManager();
+             
+        // Creating Contact entity that will be save to the sqlite database
+        MonitoringTestSeries myContact = new MonitoringTestSeries(3, "My Name", "my_email@email.com");
+        MonitoringTestSeries yourContact = new MonitoringTestSeries(24, "Your Name", "your_email@email.com");
+           
+        em.getTransaction().begin();
+        em.persist(myContact);
+        em.getTransaction().commit();
+        
+        UtilsFacadeBean ufb = new UtilsFacadeBean(em);        
+        List<MonitoringTestSeries> contactList = ufb.all(MonitoringTestSeries.class);
+
+        for (MonitoringTestSeries contact : contactList) {
+            System.out.println("Id: " + contact.getId() + " | Name:"  + contact.getName() + " | Email:" + contact.getEmail());
+        }
+             
+        
+       
+    }
+}
