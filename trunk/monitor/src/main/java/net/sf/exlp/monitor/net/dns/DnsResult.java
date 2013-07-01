ba57a992fc4a3ccaf4682119f3b513ba74fb7f7e
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2013-07-01 13:41:05 +0000

    classes moved to utils (EXLP-17)

--- trunk/monitor/src/main/java/net/sf/exlp/monitor/net/dns/DnsResult.java
+++ trunk/monitor/src/main/java/net/sf/exlp/monitor/net/dns/DnsResult.java
@@ -2,21 +2,10 @@ package net.sf.exlp.monitor.net.dns;
 
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import net.sf.ahtutils.model.interfaces.with.EjbWithRecord;
-
-@Entity
-public class DnsResult  implements EjbWithRecord
+public class DnsResult
 {
 	public static enum Code {SUCCESSFUL,UNRECOVERABLE,TRY_AGAIN,HOST_NOT_FOUND,TYPE_NOT_FOUND}
 	
-	
-	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-	private long id;
 		
 	private long duration;
 		
@@ -25,9 +14,6 @@ public class DnsResult  implements EjbWithRecord
 	private Code code;
 	
 	// ********   Getter/Setter   *********** //
-	
-	public long getId() {return id;}
-	public void setId(long id) {this.id = id;}
 		 
 	public long getDuration() {return duration;}
 	public void setDuration(long duration) {this.duration = duration;}
@@ -42,7 +28,6 @@ public class DnsResult  implements EjbWithRecord
 	public String toString()
 	{
 		StringBuffer sb = new StringBuffer();
-		sb.append("(").append(id).append(")");
 		sb.append(" ").append(code.toString());
 		sb.append(" ").append(record);
 		sb.append(" ").append(duration);
