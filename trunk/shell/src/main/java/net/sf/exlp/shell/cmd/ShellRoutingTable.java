1e8b45b4410fdb0d42a7817ad2ef6793aec52905
thorsten-k <thorsten-k@d673c977-b11e-434f-8d9e-11a1b75de072>
2013-07-04 14:13:16 +0000

    shell packages restructured (EXLP-20)

--- trunk/shell/src/main/java/net/sf/exlp/shell/cmd/ShellRoutingTable.java
+++ trunk/shell/src/main/java/net/sf/exlp/shell/cmd/ShellRoutingTable.java
@@ -1,7 +1,7 @@
-package net.sf.exlp.util.os.shell;
+package net.sf.exlp.shell.cmd;
 
 import net.sf.exlp.exception.ExlpUnsupportedOsException;
-import net.sf.exlp.util.os.ArchUtil;
+import net.sf.exlp.shell.util.ArchUtil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- trunk/shell/src/main/java/net/sf/exlp/shell/util/OsTempDirFactory.java
+++ trunk/shell/src/main/java/net/sf/exlp/shell/util/OsTempDirFactory.java
@@ -1,8 +1,9 @@
-package net.sf.exlp.util.os;
+package net.sf.exlp.shell.util;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+
 
 import org.apache.commons.lang.SystemUtils;
 import org.slf4j.Logger;
