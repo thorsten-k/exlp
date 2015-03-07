05a73522ad4ce23ec0401311febb92f3a9c99f0f
thorsten-k <t.kisner@web.de>
2015-03-07 07:29:29 +0000

    ConfigruationParamter added

---
+++ util/src/main/java/net/sf/exlp/factory/txt/TxtConfigurationParamterFactory.java
@@ -0,0 +1,38 @@
+package net.sf.exlp.factory.txt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.exlp.xml.config.Parameter;
+import net.sf.exlp.xml.config.Parameters;
+
+public class TxtConfigurationParamterFactory
+{
+	public static List<String> build(Parameters paramters)
+	{
+		List<String> required = new ArrayList<String>();
+		List<String> optional = new ArrayList<String>();
+		for(Parameter p : paramters.getParameter())
+		{
+			StringBuffer sb = new StringBuffer();
+			sb.append("\t");
+			sb.append(p.getKey());
+			sb.append("=").append(p.getValue());
+			sb.append(" (").append(p.getDescription()).append(")");
+			if(p.isRequired()){required.add(sb.toString());}
+			else{optional.add(sb.toString());}
+		}
+		
+		List<String> result = new ArrayList<String>();
+		result.add("Required Paramter");
+		result.addAll(required);
+		
+		if(optional.size()>0)
+		{
+			result.add("Optional Paramter");
+			result.addAll(optional);
+		}
+		
+		return result;
+	}
+}
---
+++ util/src/main/java/net/sf/exlp/factory/xml/config/XmlParametersFactory.java
@@ -0,0 +1,12 @@
+package net.sf.exlp.factory.xml.config;
+
+import net.sf.exlp.xml.config.Parameters;
+
+public class XmlParametersFactory
+{
+	public static Parameters build()
+	{
+		Parameters xml = new Parameters();
+		return xml;
+	}
+}
