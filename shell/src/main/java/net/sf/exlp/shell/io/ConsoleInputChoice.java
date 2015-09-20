a9feeb2aeba54cc8ff55558807c8e198b0d4bb22
Thorsten <t.kisner@web.de>
2015-09-20 10:50:59 +0200

    InputChoiceReader renamed to ConsoleInputChoice

---
+++ shell/src/main/java/net/sf/exlp/shell/io/ConsoleInputChoice.java
@@ -0,0 +1,106 @@
+package net.sf.exlp.shell.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConsoleInputChoice
+{
+	final static Logger logger = LoggerFactory.getLogger(ConsoleInputChoice.class);
+	
+	public static <E extends Enum<E>> E getEnumForChoice(E[] choice) throws IllegalArgumentException
+	{
+		int index=-1;
+		String inputString=null;
+		for(int i=0;i<choice.length;i++)
+		{
+			System.out.println("\t("+(i+1)+") "+choice[i]);
+		}
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try
+		{
+			inputString = br.readLine();
+			index = Integer.parseInt(inputString);
+		}
+		catch (IOException ioe) {ioe.printStackTrace();}
+		catch (NumberFormatException nfe) {throw new IllegalArgumentException("Input '"+inputString+"' is not valid");}
+		
+		if(index<1 || index > choice.length)
+		{
+			throw new IllegalArgumentException("Input '"+inputString+"' is not valid");
+		}
+		
+		return choice[index-1]; 
+	}
+	
+	public static <T extends Object> T getListItemForChoice(List<T> list) throws IllegalArgumentException
+	{
+		Map<T,String> map = new Hashtable<T,String>();
+		for(T t : list){map.put(t, t.toString());}
+		return getListItemForChoice(list,map); 
+	}
+	
+	public static <T extends Object> T getListItemForChoice(List<T> list, Map<T,String> map) throws IllegalArgumentException
+	{
+		int index=-1;
+		String inputString=null;
+		for(int i=0;i<list.size();i++)
+		{
+			System.out.println("\t("+(i+1)+") "+map.get(list.get(i)));
+		}
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try
+		{
+			inputString = br.readLine();
+			index = Integer.parseInt(inputString);
+		}
+		catch (IOException ioe) {ioe.printStackTrace();}
+		catch (NumberFormatException nfe) {throw new IllegalArgumentException("Input '"+inputString+"' is not valid");}
+		
+		if(index<1 || index > list.size())
+		{
+			throw new IllegalArgumentException("Input '"+inputString+"' is not valid");
+		}
+		
+		return list.get(index-1); 
+	}
+	
+	public static String readLine(String out)
+	{
+		String result="";
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try
+		{
+			System.out.println(out);
+			result = br.readLine();
+		}
+		catch (IOException ioe) {ioe.printStackTrace();}
+		return result;
+	}
+	
+	public static int readInt(String out)
+	{
+		String s = readLine(out);
+		int result=0;
+		try	{result = Integer.parseInt(s);}
+		catch (NumberFormatException nfe) {throw new IllegalArgumentException("Eingabe "+s+" unzul�ssig");}
+		return result;
+	}
+	
+	public static double readDouble(String out)
+	{
+		String s = readLine(out);
+		double result=0;
+		try	{result = Double.parseDouble(s);}
+		catch (NumberFormatException nfe) {throw new IllegalArgumentException("Eingabe "+s+" unzul�ssig");}
+		return result;
+	}
+}
---
+++ shell/src/test/java/net/sf/exlp/shell/io/CliConsoleInputChoice.java
@@ -0,0 +1,50 @@
+package net.sf.exlp.shell.io;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.sf.exlp.test.AbstractExlpShellTest;
+import net.sf.exlp.test.ExlpShellTestBootstrap;
+
+public class CliConsoleInputChoice extends AbstractExlpShellTest
+{
+	final static Logger logger = LoggerFactory.getLogger(CliConsoleInputChoice.class);
+   
+	private static enum Code {a,b,c}
+	
+	public CliConsoleInputChoice()
+	{
+		
+	}
+	
+	public void testEnum()
+	{
+		Code code = ConsoleInputChoice.getEnumForChoice(Code.values());
+		logger.info("Selected: "+code);
+	}
+	
+	public void testList()
+	{
+		String s;
+		List<String> list = new ArrayList<String>();
+		Map<String,String> map = new Hashtable<String,String>();
+		s = "l1"; list.add(s);map.put(s, "Text "+s);
+		s = "l2"; list.add(s);map.put(s, "Text "+s);
+		String result = ConsoleInputChoice.getListItemForChoice(list,map);
+		logger.info("Selected: "+result);
+	}
+	
+	public static void main(String[] args)
+	{
+		ExlpShellTestBootstrap.init();
+		
+		CliConsoleInputChoice cli = new CliConsoleInputChoice();
+//		cli.testEnum();
+		cli.testList();
+	}
+}
