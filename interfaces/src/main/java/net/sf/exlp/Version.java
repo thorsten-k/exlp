f887299e2e38e24f800501172bfdc3669462272e
Thorsten <t.kisner@web.de>
2016-03-31 10:16:23 +0200

    Versions of bom-libs added

--- interfaces/src/main/java/net/sf/exlp/Version.java
+++ interfaces/src/main/java/net/sf/exlp/Version.java
@@ -2,5 +2,13 @@ package net.sf.exlp;
 
 public class Version
 {
-
+	public static final String jodaVersion = "2.9.2";
+	public static final String commonsCodecVersion = "1.10";
+	public static final String commonsLangVersion = "2.6";
+	
+	public static final String log4jVersion = "1.2.17";
+	public static final String slf4jVersion = "1.7.19";
+	
+	public static final String jdom2Version = "2.0.6";
+	public static final String jaxbVersion = "2.2.2";
 }
