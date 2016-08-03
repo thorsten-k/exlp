dc4c4cc7d6fa635f9cd5c8422ced616bfb5c8948
Thorsten <t.kisner@web.de>
2016-08-03 19:02:05 +0200

    verisons updated

--- interfaces/src/main/java/net/sf/exlp/Version.java
+++ interfaces/src/main/java/net/sf/exlp/Version.java
@@ -2,13 +2,13 @@ package net.sf.exlp;
 
 public class Version
 {
-	public static final String jodaVersion = "2.9.2";
+	public static final String jodaVersion = "2.9.4";
 	public static final String commonsCodecVersion = "1.10";
 	public static final String commonsLangVersion = "2.6";
 	public static final String commonsJxPath = "1.3";
 	
 	public static final String log4jVersion = "1.2.17";
-	public static final String slf4jVersion = "1.7.19";
+	public static final String slf4jVersion = "1.7.21";
 	
 	public static final String jdomVersion = "2.0.2";
 	public static final String jdom2Version = "2.0.6";
