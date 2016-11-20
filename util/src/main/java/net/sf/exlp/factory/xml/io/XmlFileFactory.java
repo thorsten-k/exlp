604d0b09075e26efcc1b61e0aaac58dab5c564a3
Thorsten <t.kisner@web.de>
2016-11-20 07:03:35 +0100

    XmlFileFactory improved

--- util/src/main/java/net/sf/exlp/factory/xml/io/XmlFileFactory.java
+++ util/src/main/java/net/sf/exlp/factory/xml/io/XmlFileFactory.java
@@ -1,15 +1,33 @@
 package net.sf.exlp.factory.xml.io;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.zip.ZipEntry;
+
+import org.apache.commons.io.IOUtils;
 
 import net.sf.exlp.xml.io.File;
 
 public class XmlFileFactory
 {
+	public static File build(java.io.File f) throws FileNotFoundException, IOException
+	{
+		return build(f.getName(),IOUtils.toByteArray(new FileInputStream(f)));
+	}
+	
 	public static File build(String name)
 	{
 		File xml = new File();
 		xml.setName(name);
+		return xml;
+	}
+	
+	public static File build(String name, byte[] bytes)
+	{
+		File xml = new File();
+		xml.setName(name);
+		xml.setData(XmlDataFactory.build(bytes));
 		return xml;
 	}
 	
