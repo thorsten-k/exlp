01744cf01e77e81d59cfd3d627c6e2de6065aa97
Thorsten <t.kisner@web.de>
2016-11-21 10:02:02 +0100

    XmlFilesFactory improved

--- util/src/main/java/net/sf/exlp/factory/xml/io/XmlFilesFactory.java
+++ util/src/main/java/net/sf/exlp/factory/xml/io/XmlFilesFactory.java
@@ -1,5 +1,8 @@
 package net.sf.exlp.factory.xml.io;
 
+import java.util.List;
+
+import net.sf.exlp.xml.io.File;
 import net.sf.exlp.xml.io.Files;
 
 public class XmlFilesFactory
@@ -8,6 +11,13 @@ public class XmlFilesFactory
 	{
 		Files xml = new Files();
 
+		return xml;
+	}
+	
+	public static Files build(List<File> files)
+	{
+		Files xml = build();
+		if(files!=null){xml.getFile().addAll(files);}
 		return xml;
 	}
 }
