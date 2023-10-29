d5ee23dd8974ad024de23a2895c7b2e4999a99b5
Thorsten-K <t.kisner@web.de>
2023-10-29 07:28:26 +0200

    JSFUtil added

---
+++ util/src/main/java/org/exlp/util/jsf/JsfUtil.java
@@ -0,0 +1,24 @@
+package org.exlp.util.jsf;
+
+import javax.faces.component.UIOutput;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JsfUtil
+{
+	final static Logger logger = LoggerFactory.getLogger(JsfUtil.class);
+	
+	public static void pushJsToHead(FacesContext context, String library, String name) throws AbortProcessingException
+	{		
+		UIOutput js = new UIOutput();
+		js.setRendererType("javax.faces.resource.Script");
+		js.getAttributes().put("library", library);
+		js.getAttributes().put("name", name);
+		
+		context.getViewRoot().addComponentResource(context, js, "head");
+		logger.info("Added " +name +" to head.");
+	}
+}
---
+++ util/src/main/java/org/exlp/util/jsf/jx/ComponentAttribute.java
@@ -0,0 +1,46 @@
+package org.exlp.util.jsf.jx;
+
+import java.util.Map;
+import java.util.Objects;
+
+import javax.el.ValueExpression;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ComponentAttribute
+{
+	final static Logger logger = LoggerFactory.getLogger(ComponentAttribute.class);
+	
+	public static <E extends Enum<E>> String toString(FacesContext ctx, UIComponent component, E attribute)
+	{
+		Object o = ComponentAttribute.toObject(ctx, component, attribute);
+		if(Objects.isNull(o)) {return null;}
+		else {return o.toString();}
+	}
+	
+	private static <E extends Enum<E>> Object toObject(FacesContext ctx, UIComponent component, E attribute) {return ComponentAttribute.toObject(ctx, component, attribute.toString(), null);}
+	private static Object toObject(FacesContext ctx, UIComponent component, String attribute, Object defaultValue)
+	{
+		Object value = null;
+		if(component.getAttributes().containsKey(attribute))
+		{
+			value = component.getAttributes().get(attribute);
+			logger.info("Value from "+component.getClass().getSimpleName()+"."+Map.class.getSimpleName()+": "+value);
+		}
+		else
+		{
+			ValueExpression ve = component.getValueExpression(attribute);
+			if(Objects.nonNull(ve))
+			{
+				value=ve.getValue(ctx.getELContext());
+			}
+			logger.info("Value from "+ValueExpression.class.getSimpleName()+": "+value);
+		}
+		if(Objects.isNull(value)) {value=defaultValue;}
+		logger.info("Value Final: "+value);
+		return value;
+	}
+}
